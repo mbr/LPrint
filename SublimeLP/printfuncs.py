8916563fc4c774b108c75434b8351fdc58ed19b3
Marc Brinkmann <git@marcbrinkmann.de>
2015-03-02 12:20:43 +0100

    Allow per-filetype settings changes.

--- SublimeLP/printfuncs.py
+++ SublimeLP/printfuncs.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 import sublime
@@ -6,43 +7,66 @@ import sublime_plugin
 from .printing.lp import PrintSystemLP
 
 
+def _get_synax(view):
+    syntax_file = view.settings().get('syntax')
+    return os.path.splitext(os.path.basename(syntax_file))[0]
+
+
 class SettingsAdapter(object):
-    def __init__(self, settings):
+    """Wraps multiple sublime settings in a dict-like interface.
+
+    Settings are consulted in order. Any removal of keys is tracked as well.
+    """
+    def __init__(self, settings=[]):
         self.settings = settings
-        self.popped = []
+        self.popped = set()
         self.added = {}
+
+    def copy(self):
+        cp = SettingsAdapter(self.settings)
+        cp.added = self.added.copy()
+        cp.popped = self.popped[:]
+
+        return cp
 
     def __setitem__(self, key, value):
         self.added[key] = value
+        self.popped.discard(key)
 
     def __getitem__(self, key):
         return self.get(key)
 
     def __contains__(self, key):
-        return self.settings.has(key)
+        try:
+            self.get(key)
+            return True
+        except KeyError:
+            return False
 
     def get(self, key, *args):
-        if key in self.popped:
-            if len(args) < 1:
-                raise KeyError(key)
+        assert len(args) < 2  # one optional arg
+
+        if key not in self.popped:
+            if key in self.added:
+                return self.added[key]
+
+            for s in self.settings:
+                if s.has(key):
+                    return s.get(key)
+
+        if len(args) > 0:
             return args[0]
 
-        if key in self.added:
-            return self.added[key]
-
-        return self.settings.get(key, *args)
+        raise KeyError(key)
 
     def pop(self, *args):
         val = self.get(*args)
-        self.popped.append(args[0])
+        self.popped.add(args[0])
 
         return val
 
 
 class PrintUsingDefaultPrinterCommand(sublime_plugin.TextCommand):
-    def load_settings(self):
-        return sublime.load_settings('SublimeLP.sublime-settings')
-
     def run_lp(self, lines, title):
         try:
             settings = self.settings
@@ -116,7 +140,13 @@ class PrintUsingDefaultPrinterCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         content = self.view.substr(sublime.Region(0, self.view.size()))
-        options = SettingsAdapter(self.load_settings())
+
+        options = SettingsAdapter([
+            sublime.load_settings(
+                'SublimeLP-{}'.format(_get_synax(self.view))
+            ),
+            sublime.load_settings('SublimeLP.sublime-settings'),
+        ])
 
         # instantiate printing system
         ps = PrintSystemLP()
@@ -132,6 +162,6 @@ class PrintUsingDefaultPrinterCommand(sublime_plugin.TextCommand):
         title = (self.view.name() or self.view.file_name() or
                  'Buffer {}'.format(self.view.buffer_id()))
         options['title'] = title
-        sublime.status_message('Printing {}'.format(title))
+        sublime.status_message('Printing ({}): {}'.format(printer.name, title))
 
         printer.print_raw(content.encode('utf8'), options)
